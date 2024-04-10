d5df4ef78907f07e5346a0ec9d4712ef020a02e4
Mun <156924828+PillowsWannaCry@users.noreply.github.com>
2024-04-10 23:48:33 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+// main.cpp
+
+#include <iostream>
+#include "Fraction.h"
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int test_op;
+    cin >> test_op;
+    switch (test_op) {
+    case 1:
+        cout << (Fraction(1, 2) == Fraction(1, 3)) << endl;
+        cout << (Fraction(1, 2) == Fraction(2, 4)) << endl;
+        break;
+    case 2:
+        cout << (Fraction(1, 2) != Fraction(1, 3)) << endl;
+        cout << (Fraction(1, 2) != Fraction(2, 4)) << endl;
+        break;
+    case 3:
+        cout << (Fraction(1, 2) > Fraction(1, 3)) << endl;
+        cout << (Fraction(1, 2) > Fraction(2, 4)) << endl;
+        break;
+    case 4:
+        cout << (Fraction(1, 2) < Fraction(1, 3)) << endl;
+        cout << (Fraction(1, 2) < Fraction(2, 4)) << endl;
+        break;
+    }
+    return 0;
+}
